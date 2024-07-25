8d5339ea89bb35aa9955f33e6b80b64afe857b4f
Zimizig <ramazanow9999@gmail.com>
2024-07-25 15:54:53 +0500

    first commit add table view

---
+++ EmojiReader/EmojiTableViewController.swift
@@ -0,0 +1,15 @@
+//
+//  EmojiTableViewController.swift
+//  EmojiReader
+//
+//  Created by Роман on 25.07.2024.
+//
+
+import UIKit
+
+class EmojiTableViewController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.backgroundColor = .brown
+    }
+}
