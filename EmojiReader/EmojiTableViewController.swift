c328d09e2de18104c4f02b153c5bc483d1593cdb
Zimizig <ramazanow9999@gmail.com>
2024-07-29 21:40:02 +0500

    added navigation bar

--- EmojiReader/EmojiTableViewController.swift
+++ EmojiReader/EmojiTableViewController.swift
@@ -10,6 +10,30 @@ import UIKit
 class EmojiTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.title = "Emoji Reader"
+        self.navigationItem.leftBarButtonItem = self.editButtonItem
+        
         tableView.backgroundColor = .brown
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "\(indexPath)"
+        return cell
+        
+    }
+    
 }
